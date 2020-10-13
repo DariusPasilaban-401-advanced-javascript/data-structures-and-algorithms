4d53e78fc5636b5412c947d1d863bef1d1bdfcc9
Darius Lee Pasilaban <p.dariuslee@gmail.com>
2020-10-13 13:51:02 -0700

    code challenge 1 - array-reverse

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # data-structures-and-algorithms
+
+## Table of Contents
+
+### Code-Challenge
+
+1:[array-reverse]()
+
+2:
+
+3:
---
+++ arrayReverse/array-reverse.js
@@ -0,0 +1,11 @@
+
+let arr1 = [1,2,3];
+let arr2 = ['alpha', 'bravo', 'charlie']
+
+function reverseArray(arr){
+  let result = arr.reverse();
+  console.log(result);
+}
+
+reverseArray(arr1);
+reverseArray(arr2);
